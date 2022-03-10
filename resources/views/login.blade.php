1a792764a357fb98b6983c7d809acccb20c9f53a
amaliaanjani <amaliaanjani821@gmail.com>
2022-03-10 14:13:22 +0700

    commit kedua

---
+++ resources/views/login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+	
+<link rel="stylesheet" href="/css/style.css"> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Laravel</title> 
+    <!doctype html> 
+    <!DOCTYPE html>
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="order">
+				<form>
+					<label for="chk" aria-hidden="true">Pesan Tiket</label>
+	<font color="white">	<center> <h3>Data Diri</h3></font> </center>
+					<input type="text" name="txt" placeholder="Nama" required="">
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="No Telfon" name="number" placeholder="No Telfon" required="">
+
+					<font color="white">	<center> <h3>Tujuan Anda</h3></font> </center>
+					<input type="text" name="txt" placeholder="Masukkan Tujuan" required="">
+					<input type="date" name="Pergi" placeholder="Pergi" required="">
+					<input type="date" name="Pulang" placeholder="Pulang" required="">
+					<button>Pesan</button>
+				</form>  
+			</div>
+
+			
+	</div>
+</body>
+</html>
