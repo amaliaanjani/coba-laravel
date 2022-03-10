b5502b8ae1f40427a0a57395ccadeede973d8b35
amaliaanjani <amaliaanjani821@gmail.com>
2022-03-10 13:25:54 +0700

    first commit

---
+++ resources/views/konfirmasi.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+	
+<link rel="stylesheet" href="/css/style.css"> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>LaravelKonfirmasi</title> 
+    <!doctype html> 
+    <!DOCTYPE html>
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<div class="main">  	
+			<div class="konfirmasi order">
+                    <font color="black">	<center> <h1>Konfirmasi Order</h1></font> </center>
+                    <form action="/action_page.php">
+  <label for="fname">Nama:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Paket yang dipesan:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <label for="lname">Email:</label><br>
+  <input type="email" id="lname" name="lname" value=""><br><br>
+  <label for="lname">No.Rekening Bank:</label><br>
+  <input type="number" id="lname" name="lname" value=""><br><br>
+  <label for="lname">Tanggal Transfer:</label><br>
+  <input type="date" id="lname" name="lname" value=""><br><br>
+  <label for="lname">Upload:</label><br>
+  <input type="file" id="lname" name="lname" value=""><br><br>
+</form> 
+					<button>Konfirmasi</button>
+				</form>  
+			</div>
+
+			
+	</div>
+</body>
+</html>
